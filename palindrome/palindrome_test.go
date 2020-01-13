8f48cda7724c057d11318bca031172b51fa6824e
Adam S Levy <adam@aslevy.com>
2020-01-13 10:17:09 -0900

    Add false palindrome test

--- palindrome/palindrome_test.go
+++ palindrome/palindrome_test.go
@@ -34,11 +34,46 @@ var palindromes = []string{
 	"Dennis sinned",
 	"Never odd or even.",
 }
+var jabberwocky = []string{
+	"’Twas brillig, and the slithy toves",
+	"Did gyre and gimble in the wabe:",
+	"All mimsy were the borogoves,",
+	"And the mome raths outgrabe.",
+	"“Beware the Jabberwock, my son!",
+	"The jaws that bite, the claws that catch!",
+	"Beware the Jubjub bird, and shun",
+	"The frumious Bandersnatch!”",
+	"He took his vorpal sword in hand;",
+	"Long time the manxome foe he sought—",
+	"So rested he by the Tumtum tree",
+	"And stood awhile in thought.",
+	"And, as in uffish thought he stood,",
+	"The Jabberwock, with eyes of flame,",
+	"Came whiffling through the tulgey wood,",
+	"And burbled as it came!",
+	"One, two! One, two! And through and through",
+	"The vorpal blade went snicker-snack!",
+	"He left it dead, and with its head",
+	"He went galumphing back.",
+	"“And hast thou slain the Jabberwock?",
+	"Come to my arms, my beamish boy!",
+	"O frabjous day! Callooh! Callay!”",
+	"He chortled in his joy.",
+	"’Twas brillig, and the slithy toves",
+	"Did gyre and gimble in the wabe:",
+	"All mimsy were the borogoves,",
+	"And the mome raths outgrabe.",
+}
 
 func TestIs(t *testing.T) {
 	for _, phrase := range palindromes {
 		if !Is(phrase) {
 			t.Fatalf("Is(%q) returned false but should be true", phrase)
+		}
+	}
+	for _, line := range jabberwocky {
+		if Is(line) {
+			t.Fatalf("Is(%q) returned true but should be false", line)
 		}
 	}
 }
